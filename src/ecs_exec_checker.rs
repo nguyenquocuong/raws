47a6784ff06b1f039bb3f9a446696d35afd0c570
Cuong Nguyen <nguyenquocuong.it@gmail.com>
2024-07-23 11:53:36 +0700

    check ecs:ExecuteCommand

--- src/ecs_exec_checker.rs
+++ src/ecs_exec_checker.rs
@@ -1,4 +1,5 @@
 use aws_sdk_ecs::types::ClusterField;
+use aws_sdk_iam::types::PolicyEvaluationDecisionType;
 use clap::Args;
 use colored::Colorize;
 
@@ -19,14 +20,18 @@ impl EcsExecChecker {
         println!("{self:?}");
 
         let config = aws_config::load_from_env().await;
+        let sts_client = aws_sdk_sts::Client::new(&config);
+        let iam_client = aws_sdk_iam::Client::new(&config);
+        let ecs_client = aws_sdk_ecs::Client::new(&config);
 
         println!("Region : {}", config.region().unwrap());
         println!("Cluster: {}", self.cluster_name);
         println!("Task   : {}", self.ecs_task_id);
 
-        println!("Check cluster configurations");
-        let ecs_client = aws_sdk_ecs::Client::new(&config);
+        println!("Get caller identity");
+        let caller_identity = sts_client.get_caller_identity().send().await.unwrap();
 
+        println!("Check cluster configurations");
         let clusters = ecs_client
             .describe_clusters()
             .clusters(self.cluster_name.clone())
@@ -49,5 +54,31 @@ impl EcsExecChecker {
                 todo!()
             }
         }
+
+        println!("Can I ExecuteCommand? {}", caller_identity.arn().unwrap());
+        println!(
+            "\tecs:ExecuteCommand: {}",
+            match iam_client
+                .simulate_principal_policy()
+                .policy_source_arn(caller_identity.arn().unwrap())
+                .action_names("ecs:ExecuteCommand")
+                .resource_arns(format!(
+                    "arn:aws:ecs:{}:{}:task/{}/{}",
+                    config.region().unwrap(),
+                    caller_identity.account().unwrap(),
+                    self.cluster_name,
+                    self.ecs_task_id
+                ))
+                .send()
+                .await
+                .unwrap()
+                .evaluation_results()[0]
+                .eval_decision()
+            {
+                PolicyEvaluationDecisionType::Allowed =>
+                    PolicyEvaluationDecisionType::Allowed.to_string().green(),
+                decision => decision.to_string().yellow(),
+            }
+        )
     }
 }
