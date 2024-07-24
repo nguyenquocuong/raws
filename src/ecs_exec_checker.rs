ece4fad16bcf029c69550d9abc237207e7f1ac22
Cuong Nguyen <nguyenquocuong.it@gmail.com>
2024-07-24 17:11:33 +0700

    check basic ecs task

--- src/ecs_exec_checker.rs
+++ src/ecs_exec_checker.rs
@@ -1,7 +1,8 @@
-use aws_sdk_ecs::types::ClusterField;
+use aws_sdk_ecs::types::{ClusterField, LaunchType};
 use aws_sdk_iam::types::PolicyEvaluationDecisionType;
 use clap::Args;
 use colored::Colorize;
+use version_compare::{compare_to, Cmp};
 
 #[derive(Debug, Args)]
 #[command(version, about, long_about = None)]
@@ -44,16 +45,31 @@ impl EcsExecChecker {
             panic!("{:#?}", clusters.failures());
         }
 
-        match clusters.clusters()[0].configuration() {
+        let cluster = clusters.clusters().first().unwrap();
+        match cluster.configuration() {
             None => println!(
                 "Cluster configuration: {}",
                 "Audit Logging Not Configured".yellow()
             ),
             Some(_) => {
-                println!("{:#?}", clusters.clusters());
+                println!("{:#?}", cluster);
                 todo!()
             }
         }
+
+        let ecs_tasks = ecs_client
+            .describe_tasks()
+            .cluster(self.cluster_name.clone())
+            .tasks(self.ecs_task_id.clone())
+            .send()
+            .await
+            .unwrap();
+        if !ecs_tasks.failures().is_empty() {
+            println!("The specified ECS task does not exist.");
+            panic!("{:#?}", ecs_tasks.failures());
+        }
+
+        let ecs_task = ecs_tasks.tasks().first().unwrap();
 
         println!("Can I ExecuteCommand? {}", caller_identity.arn().unwrap());
         println!(
@@ -72,7 +88,9 @@ impl EcsExecChecker {
                 .send()
                 .await
                 .unwrap()
-                .evaluation_results()[0]
+                .evaluation_results()
+                .first()
+                .unwrap()
                 .eval_decision()
             {
                 PolicyEvaluationDecisionType::Allowed =>
@@ -96,13 +114,80 @@ impl EcsExecChecker {
                 .send()
                 .await
                 .unwrap()
-                .evaluation_results()[0]
+                .evaluation_results()
+                .first()
+                .unwrap()
                 .eval_decision()
             {
                 PolicyEvaluationDecisionType::Allowed =>
                     PolicyEvaluationDecisionType::Allowed.to_string().yellow(),
                 decision => decision.to_string().green(),
             }
-        )
+        );
+
+        let task_status = ecs_task.last_status().unwrap();
+        let launch_type = ecs_task.launch_type().unwrap();
+        println!(
+            "{: <21}: {}",
+            "Task Status",
+            match task_status {
+                "RUNNING" => task_status.green(),
+                "PROVISIONING" | "ACTIVATING" | "PENDING" => task_status.yellow(),
+                "DEACTIVATING" | "STOPPING" | "DEPROVISIONING" => task_status.red(),
+                "STOPPED" => format!(
+                    "{} ({})",
+                    task_status.red(),
+                    ecs_task.stopped_reason().unwrap()
+                )
+                .to_string()
+                .into(),
+                _ => task_status.red(),
+            }
+        );
+
+        println!(
+            "{: <21}: {}",
+            "Launch Type",
+            match launch_type {
+                LaunchType::Fargate | LaunchType::Ec2 => launch_type.to_string().green(),
+                _ => launch_type.to_string().yellow(),
+            }
+        );
+
+        match launch_type {
+            LaunchType::Fargate => {
+                let platform_family = ecs_task.platform_family().unwrap();
+                let require_platform_version = match platform_family {
+                    _ if platform_family.contains("Windows") => "1.0.0",
+                    _ => "1.4.0",
+                };
+
+                let platform_version = ecs_task.platform_version().unwrap();
+                println!(
+                    "{: <21}: {}",
+                    "Platform Version",
+                    match compare_to(platform_version, require_platform_version, Cmp::Ge).unwrap() {
+                        true => platform_version.green(),
+                        false => format!(
+                            "{} (Required: >= {})",
+                            platform_version,
+                            require_platform_version.red()
+                        )
+                        .to_string()
+                        .into(),
+                    }
+                );
+            }
+            _ => todo!(),
+        };
+
+        println!(
+            "{: <21}: {}",
+            "Exec Command Enabled?",
+            match ecs_task.enable_execute_command {
+                true => "OK".green(),
+                false => "NO".red(),
+            }
+        );
     }
 }
